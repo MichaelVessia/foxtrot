a6d5830341e94545982603ce5754283fa9a6b1ad
Michael Vessia <michael@vessia.net>
2023-11-26 17:56:01 -0500

    simplify

--- foxtrot.go
+++ foxtrot.go
@@ -1,67 +1,80 @@
 package main
 
 import (
-  "os"
-  "log"
-  "fmt"
-  "net/http"
-  "strings"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 var natoMap = map[string]string{
-  "a": "Alpha", "b": "Bravo", "c": "Charlie", "d": "Delta",
-  "e": "Echo", "f": "Foxtrot", "g": "Golf", "h": "Hotel",
-  "i": "India", "j": "Juliett", "k": "Kilo", "l": "Lima",
-  "m": "Mike", "n": "November", "o": "Oscar", "p": "Papa",
-  "q": "Quebec", "r": "Romeo", "s": "Sierra", "t": "Tango",
-  "u": "Uniform", "v": "Victor", "w": "Whiskey", "x": "X-ray",
-  "y": "Yankee", "z": "Zulu",
+	"a": "Alpha", "b": "Bravo", "c": "Charlie", "d": "Delta",
+	"e": "Echo", "f": "Foxtrot", "g": "Golf", "h": "Hotel",
+	"i": "India", "j": "Juliett", "k": "Kilo", "l": "Lima",
+	"m": "Mike", "n": "November", "o": "Oscar", "p": "Papa",
+	"q": "Quebec", "r": "Romeo", "s": "Sierra", "t": "Tango",
+	"u": "Uniform", "v": "Victor", "w": "Whiskey", "x": "X-ray",
+	"y": "Yankee", "z": "Zulu",
 }
 
 func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Current working directory:", cwd)
 
-  cwd, err := os.Getwd()
-  if err != nil {
-    log.Fatal(err)
-  }
-  log.Println("Current working directory:", cwd)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8080"
-  }
+	// Serve files from the public directory
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-  http.HandleFunc("/", fileServerHandler)
-  http.HandleFunc("/nato/", natoHandler)
+	http.HandleFunc("/", fileServerHandler)
+	http.HandleFunc("/nato/", natoHandler)
 
-  fmt.Println("Server is running!")
-  http.ListenAndServe(":8080", nil)
+	fmt.Println("Server is running on port " + port)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func fileServerHandler(w http.ResponseWriter, r *http.Request) {
-  if r.URL.Path == "/" {
-    http.ServeFile(w, r, "index.html")
-  } else {
-    http.NotFound(w, r)
-  }
+	// Serve index.html for the root path
+	if r.URL.Path == "/" {
+		http.ServeFile(w, r, "public/index.html")
+		return
+	}
+
+	// Construct the path to the file in the public directory
+	filePath := "public" + r.URL.Path
+
+	// Check if the file exists and is not a directory
+	if info, err := os.Stat(filePath); err == nil && !info.IsDir() {
+		http.ServeFile(w, r, filePath)
+	} else {
+		// File not found or is a directory, return 404
+		http.NotFound(w, r)
+	}
 }
 
 func natoHandler(w http.ResponseWriter, r *http.Request) {
-  word := strings.ToLower(r.URL.Query().Get("word"))
-  var natoWords []string
-  for _, char := range word {
-    if char < 'a' || char > 'z' {
-      http.Error(w, "Invalid input", http.StatusBadRequest)
-      return
-    }
-    natoWord, exists := natoMap[string(char)]
-    if !exists {
-      http.Error(w, "Not Found", http.StatusNotFound)
-      return
-    }
-    natoWords = append(natoWords, natoWord)
-  }
-  response := strings.Join(natoWords, "\n")
-  fmt.Fprint(w, response)
+	word := strings.ToLower(r.URL.Query().Get("word"))
+	var natoWords []string
+	for _, char := range word {
+		if char < 'a' || char > 'z' {
+			http.Error(w, "Invalid input", http.StatusBadRequest)
+			return
+		}
+		natoWord, exists := natoMap[string(char)]
+		if !exists {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+		natoWords = append(natoWords, natoWord)
+	}
+	response := strings.Join(natoWords, "<br />")
+	fmt.Fprint(w, response)
 }
-
